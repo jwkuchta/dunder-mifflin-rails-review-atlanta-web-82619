1c8bb3a33fd14f50b79e7b8530ed2cc4af9af4b2
Justina Kuchta <justyna.kuchta@gmail.com>
2019-09-25 15:36:17 -0400

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,32 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        # @employees = Employee.all.select {|ee| ee.dog_id == self.id}
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+    def update
+        @dog = Dog.update(dog_params)
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
 end
